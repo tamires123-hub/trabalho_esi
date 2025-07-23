587d26f03f6b1c32a7833f0db73197158564c63b
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2025-07-23 15:05:06 +0000

    Novas alterações

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,33 @@
 import os
-import json
+import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def autentica_kaggle():
-    os.makedirs(os.path.expanduser("~/.kaggle"), exist_ok=True)
-    with open(os.path.expanduser("~/.kaggle/kaggle.json"), "w") as f:
-        json.dump(kaggle_token, f)
+def api_kaggle(datasets: list) -> None:
+    api = KaggleApi()
+    api.authenticate()
+    os.makedirs('banco_de_dados', exist_ok=True)
+
+    for df in datasets:
+        api.dataset_download_files(
+            df,
+            path = 'banco_de_dados',
+            unzip = True
+        )
+    return
+
+def lendo_df():
+    datasets = os.listdir('banco_de_dados')
+    dt = {}
+    for df in datasets:
+        caminho = os.path.join('banco_de_dados', df)
+        df = pd.read_csv(caminho)
+        nome = os.path.splitxt(df)[0]
+        dt[nome] = df
+
+    return dt
+
+datasets = [
+    '/ashpalsingh1525/imdb-movies-dataset',
+    '/willianoliveiragibin/10000-data-about-movies-1915-2023'
+]
+
